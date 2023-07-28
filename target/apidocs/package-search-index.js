5e38602fbd99419942c7bc4490f5107ed4f306e7
IPolonio <122845674+IPolonio@users.noreply.github.com>
2023-07-27 21:22:29 -0400

    Delete target/apidocs directory

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.finalp"},{"l":"com.pantalla.db"},{"l":"com.pantalla.interfaces"},{"l":"com.pantalla.modelos"}];updateSearchResults();
